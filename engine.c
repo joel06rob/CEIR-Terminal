9c82708e77d98114caf9e7d9fbf6ae69e1fb70ef
joel06rob <joel.roberts2006@gmail.com>
2026-03-21 17:02:47 +0000

    logging - data logging (cruise + stress tests)

--- engine.c
+++ engine.c
@@ -15,7 +15,7 @@ void run_idle(){
     FILE *file = fopen("idle_log.csv", "w");
     if(file == NULL){
         printf("*ERROR* when opening idle_file.csv");
-        
+
     }
     fprintf(file, "%s", "TIME,RPM,TEMP,OIL\n");
     printf("TIME   RPM   TEMP   OIL\n------------------------\n");
@@ -37,11 +37,17 @@ void run_idle(){
 }
 
 void run_cruise(){
-    //Run on a typical rate between 2500 - 3000
+    //Run on a typical rate between 2500 - 3500
     printf("\nSTARTING CRUISE TEST...\n");
 
+    //Configure logging + printing for data
     EngineData data;
-
+    FILE *file = fopen("cruise_log.csv", "w");
+    if(file == NULL){
+        printf("*ERROR* when opening cruise_file.csv");
+        
+    }
+    fprintf(file, "%s", "TIME,RPM,TEMP,OIL\n");
     printf("TIME   RPM   TEMP   OIL\n------------------------\n");
 
     for(int i = 0; i < 10; i++){
@@ -50,16 +56,27 @@ void run_cruise(){
         data.oil = random_Data(30, 40);
 
         print_data(i, &data);
+        log_data(file, i, &data);
     }
+
+    fclose(file);
+
 }
 
 void run_stress(){
     //Get highly random values - Use previous value and add more to it (Ascending RPM)
     printf("\nSTARTING CRUISE TEST...\n");
 
+    //Configure logging + printing for data
     EngineData data;
-
+    FILE *file = fopen("stress_log.csv", "w");
+    if(file == NULL){
+        printf("*ERROR* when opening stress_file.csv");
+        
+    }
+    fprintf(file, "%s", "TIME,RPM,TEMP,OIL\n");
     printf("TIME   RPM   TEMP   OIL\n------------------------\n");
+
     int previous_RPM = 2700;
     int previous_Temp = 85;
 
@@ -73,7 +90,11 @@ void run_stress(){
         data.oil = random_Data(30,40);
 
         print_data(i, &data);
+        log_data(file, i, &data);
     }
+
+    fclose(file);
+
 }
 
 
