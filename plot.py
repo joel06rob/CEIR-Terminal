53b745897f1b0f3cfd570b75483a866500081462
joel06rob <joel.roberts2006@gmail.com>
2026-03-27 20:39:08 +0000

    plot - seperate into multiple graphs

--- plot.py
+++ plot.py
@@ -3,15 +3,26 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('idle_log.csv')
 
+#Create multiple graphs using subplots
+fig, axs = plt.subplots(3)
+
 #Plot data onto graph
 #plot(x axis, y axis)
-plt.plot(data["TIME"], data["RPM"], label="RPM")
-plt.plot(data["TIME"], data["TEMP"], label="TEMP (C)")
-plt.plot(data["TIME"], data["OIL"], label="OIL (PSI)")
+axs[0].plot(data["TIME"], data["RPM"], label="RPM", color="blue")
+axs[1].plot(data["TIME"], data["TEMP"], label="TEMP (C)", color="orange")
+axs[2].plot(data["TIME"], data["OIL"], label="OIL (PSI)", color="black")
 
-plt.xlabel("Time")
-plt.ylabel("Engine Data")
-plt.legend()
+
+axs[0].set_ylabel("Revs Per Minute (RPM)")
+axs[0].set_xlabel("Time")
+axs[0].legend()
+axs[1].set_ylabel("Temperature (C)")
+axs[1].set_xlabel("Time")
+axs[1].legend()
+axs[2].set_ylabel("Oil Level (PSI)")
+axs[2].set_xlabel("Time")
+axs[2].legend()
+
 
 plt.savefig("plotoutput.png")
 
